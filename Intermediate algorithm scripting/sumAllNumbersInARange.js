5cda2a5743e1d88d9d6cac1364de8a0c0c816764
tienpham94 <r3dsky02@yahoo.com.vn>
2017-07-18 10:38:31 +0300

    Finished sum all nums

---
+++ Intermediate algorithm scripting/sumAllNumbersInARange.js
@@ -0,0 +1,15 @@
+
+function sumAll(arr) {
+  var sum = 0;
+  arr.sort(function(a,b){
+    return a - b;
+  });
+
+  for(var i = arr[0];i<=arr[1];i++){
+    sum += i;
+  }
+
+  return sum;
+}
+
+sumAll([14,10 ]);
