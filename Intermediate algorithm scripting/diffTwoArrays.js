22d3af757c012c6fb47027b8d02d34dcf0260bec
tienpham94 <r3dsky02@yahoo.com.vn>
2017-07-19 22:24:18 +0300

    Refactored diffTwoArr

--- Intermediate algorithm scripting/diffTwoArrays.js
+++ Intermediate algorithm scripting/diffTwoArrays.js
@@ -1,20 +1,19 @@
 
 function diffArray(arr1, arr2) {
   var result = [];
-  for(var i = 0; i< arr1.length; i++){
-    if(arr2.indexOf(arr1[i])<0 && arr1[i]!==undefined){
-      result.push(arr1[i]);
-    }
-  }
+  return result.concat(diff(arr1,arr2),diff(arr2,arr1));
+  // Same, same; but different.
+}
 
+function diff(arr1,arr2){
+  var result = [];
   for(var j = 0; j< arr2.length; j++){
-    if(arr1.indexOf(arr2[j])<0 && arr2[j]!==undefined){
+    if(arr1.indexOf(arr2[j])<0){
       result.push(arr2[j]);
     }
   }
-
-  // Same, same; but different.
   return result;
 }
+
 console.log(
 diffArray([1, "calf", 3, "piglet"], [7, "filly"]));
